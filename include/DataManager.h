fd9c31bd5fa9bb75382963e590db58b583cb0220
yilizhang995 <yili.zhang@richmond.edu>
2024-12-02 21:28:51 -0500

    update data manager with robot manager and robot do task

--- include/DataManager.h
+++ include/DataManager.h
@@ -1,11 +1,15 @@
 #pragma once
 
 #include <string>
-#include <thread>  // For threading
-#include <atomic>  // For atomic variables to control the thread
+#include <vector>
+#include <thread>
+#include <mutex>
+#include <atomic>
 #include <wx/string.h>
 #include "adapters/mongo_adapter.hpp"
 #include "robot.hpp"
+#include "robot_manager.hpp"
+#include "robot_do_task.hpp"
 
 // Struct for RobotData
 struct RobotData {
@@ -31,26 +35,29 @@ public:
     std::vector<TaskData>& GetTasks();
     void AddRobot(RobotData& robot);
     void UpdateIds();
-    std::string GetIDString(); // this is for the UI to access ID easily when robot is made
+    std::string GetIDString();
     void AddTask(TaskData& task);
     robots::Robots GetAllRobotInfo(int robotId);
     void DeleteRobot(int robotId);
     std::vector<robots::Robots> GetAvailableRobots();
 
-private:
-    int GetNextAvailableRobotId();  // New method to find the next available robot ID
+    // Start and stop task update thread
+    void startTaskUpdateThread();
+    void stopTaskUpdateThread();
 
-    // Thread control members
-    void StartTaskUpdateThread();
-    void StopTaskUpdateThread();
-    void TaskUpdateLoop(); // Thread loop to update task progress
+private:
+    void taskUpdateLoop(); // Loop that runs to update tasks in a thread
+    int GetNextAvailableRobotId();
 
     int id;
     std::vector<RobotData> robots;  // Stores robot data in a local vector
     std::vector<TaskData> tasks;  // Stores task data in a local vector
     std::vector<int> ids;  // Stores robot IDs currently in the database
     adapters::Mongo_Adapter mongo_database{};  // MongoDB adapter to interact with the database
+    robots::RobotManager robot_manager_;  // Manages the list of robots
 
-    std::thread task_update_thread_;  // Thread for task updates
-    std::atomic<bool> running_{false};  // Controls the thread execution
+    // Threading related members
+    std::thread task_update_thread_;
+    std::atomic<bool> keep_running_; // Controls the running state of the thread
+    std::mutex robot_mutex_; // Mutex to protect shared resources
 };
