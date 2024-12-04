22b7c811f5efc8d9d6ced0d49b3122c8eaa70fae
yilizhang995 <yili.zhang@richmond.edu>
2024-12-03 19:12:50 -0500

    update thread

--- include/DataManager.h
+++ include/DataManager.h
@@ -1,12 +1,15 @@
 #pragma once
 
-// #include "../wxWidgets/Project_Implementation/feBaseFrame.hpp"  // Assuming DataManager needs access to the base frame
-// #include <sstream>
 #include <string>
-// #include "../wxWidgets/Project_Implementation/baseFrame.hpp"
+#include <vector>
+#include <thread>
+#include <atomic>
+#include <mutex>
 #include <wx/string.h>
 #include "adapters/mongo_adapter.hpp"
 #include "robot.hpp"
+#include "robot_manager.hpp"
+#include "include/robot_do_task.hpp"
 
 // struct for RobotData
 struct RobotData {
@@ -15,57 +18,51 @@ struct RobotData {
     wxString robotFunction;
 };
 
-//struct for TaskData
+// struct for TaskData
 struct TaskData {
     wxString taskRoom;
     RobotData taskRobot;
 };
 
-
 // DataManager class manages data operations between the GUI, simulation, and the MongoDB database.
 class DataManager {
 public:
     DataManager();
-  
     ~DataManager();
 
-
     void SendRobotsData(const std::vector<RobotData>& robots);
-
-    //getter function for vector of RobotData
     std::vector<RobotData>& GetRobots();
-
-    //getter function for vector of TaskData
     std::vector<TaskData>& GetTasks();
 
-    //method for adding robot (receiving robotdata from UI)
     void AddRobot(RobotData& robot);
     void UpdateIds();
-    std::string GetIDString(); //this is for the UI to access id easily when robot is made
+    std::string GetIDString();
 
-    //method for adding task (after receiving taskdata from UI)
     void AddTask(TaskData& task);
-
-    //gets complete robot info (whole robot of type robots)
     robots::Robots GetAllRobotInfo(int robotId);
-
-    //deletes a robot given ID from UI
     void DeleteRobot(int robotId);
-
-    //gets available robots for task allocation
     std::vector<robots::Robots> GetAvailableRobots();
 
-    //needs implementation!
-    //gets available rooms for task allocation
-    // std::vector<rooms::Rooms> GetAvailableRooms();
+    // Start and stop the thread to update database with task progress
+    void StartUpdateThread();
+    void StopUpdateThread();
 
 private:
-    int GetNextAvailableRobotId();  // New method to find the next available robot ID
+    int GetNextAvailableRobotId();
+    void UpdateDatabaseWithTaskProgress();  // Function for the thread to continuously update task progress in MongoDB
 
     int id;
 
-    std::vector<RobotData> robots;  // Stores robot data in a local vector
-    std::vector<TaskData> tasks;  // Stores task data in a local vector
-    std::vector<int> ids;  // Stores robot IDs currently in the database
-    adapters::Mongo_Adapter mongo_database{};  // MongoDB adapter to interact with the database
+    std::vector<RobotData> robots;
+    std::vector<TaskData> tasks;
+    std::vector<int> ids;
+
+    adapters::Mongo_Adapter mongo_database{};
+    robots::RobotManager robot_manager_;
+
+    // Thread handling
+    std::thread update_thread_;
+    std::atomic<bool> running_;
+    std::mutex robot_mutex_;  // Protect robot data during thread operations
 };
+
--- src/DataManager.cpp
+++ src/DataManager.cpp
@@ -1,55 +1,95 @@
 #include "DataManager.h"
-#include <algorithm>  // Include for std::find
+#include <algorithm>
 #include <wx/string.h>
+#include <thread>
+#include <chrono>
+#include <mutex>
+
+std::atomic<bool> stop_thread = false;
+std::mutex data_manager_mutex;
 
 DataManager::DataManager() 
 {
     // Initialize the MongoDB client and update the list of IDs
     UpdateIds();
+    // Start the thread for updating robot task progress
+    start_update_thread();
 }
 
 // Destructor: Handles cleanup, if necessary.
-DataManager::~DataManager() {}
+DataManager::~DataManager() {
+    stop_update_thread();
+}
 
-// Method to receive and process robots data
-// void DataManager::SendRobotsData(const std::vector<RobotData>& robots) {
-//     // Process each robot and prepare for database insertion or other actions
-//     for (const auto& robot : robots) {
-//         std::cout << "Size: " + robot.robotSize + ", Function: " + robot.robotFunction << std::endl;
-//     }
+// Method to start the update thread
+void DataManager::start_update_thread() {
+    update_thread_ = std::thread([this]() { update_task_progress(); });
+}
 
-//     // You can then call the database model to save or update robot data here
-// }
+// Method to stop the update thread
+void DataManager::stop_update_thread() {
+    stop_thread = true;
+    if (update_thread_.joinable()) {
+        update_thread_.join();
+    }
+}
 
+// Method to update the task progress in the database
+void DataManager::update_task_progress() {
+    while (!stop_thread) {
+        {
+            std::lock_guard<std::mutex> lock(data_manager_mutex);
+            auto& robot_list = robot_manager_.get_list();
+
+            for (auto& robot : robot_list) {
+                if (robot.get_task_status() == "Ongoing") {
+                    // Update the MongoDB database with the latest task progress, battery, and water levels
+                    mongo_database.update_task_status({robot});
+                } else if (robot.get_task_status() == "Complete") {
+                    // Update the robot status to "Available" when the task is complete
+                    robot.update_task_status("Available");
+                    mongo_database.update_task_status({robot});
+                } else if (robot.get_task_status() == "Cancelled" && robot.get_error_status().empty()) {
+                    // If a task is cancelled with no error, make the robot available
+                    robot.update_task_status("Available");
+                    mongo_database.update_task_status({robot});
+                }
+            }
+        }
+
+        // Sleep for 1 second before the next iteration
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
+}
 
 // Getter method for vector of RobotData (just ID, size, and function)
 std::vector<RobotData>& DataManager::GetRobots() {
     return robots;
 }
 
-// Getter method for vector of RobotData (just ID, size, and function)
+// Getter method for vector of TaskData (just ID, size, and function)
 std::vector<TaskData>& DataManager::GetTasks() {
     return tasks;
 }
 
 // Method to add a new robot to the system, taking the abbreviated RobotData of a robot as input
 void DataManager::AddRobot(RobotData& robot) {
-    int new_id = GetNextAvailableRobotId();  // Get a new unique ID, assigned by data manager to avoid user error
-
-    // robot.robotID = new_id;
-    // std::cout << robot.robotID << std::endl;
+    int new_id = GetNextAvailableRobotId();
 
     // Convert wxString to std::string for robotSize and robotFunction
     std::string size_str = std::string(robot.robotSize.mb_str());
     std::string function_str = std::string(robot.robotFunction.mb_str());
 
-    // Create a new robot instance with the new ID and the provided robot data
-    robots::Robots new_robot(new_id, size_str, 100, 100, "None", "Available", 0, function_str, 0);
+    // Create a new robot instance
+    robots::Robots new_robot(new_id, size_str, 100, 100, "None", "Available", Room(), function_str, 0);
     
+    // Add the new robot to the robot manager
+    robot_manager_.add_robot(new_robot);
+
     // Write the new robot to the MongoDB database
     mongo_database.write_robot(new_robot);
 
-    //update the RobotData of the added robot with its assigned ID
+    // Update RobotData of the added robot with its assigned ID
     robot.robotID = std::to_string(new_id);
     // Add the new robot data to the local list of robots
     robots.push_back(robot);
@@ -65,45 +105,37 @@ void DataManager::UpdateIds() {
 
 // Method to find the next available robot ID
 int DataManager::GetNextAvailableRobotId() {
-    id = 1;  // Start from ID 1
+    id = 1;
 
     // Find the next available ID that is not already used
     while (std::find(ids.begin(), ids.end(), id) != ids.end()) {
-        ++id;  // Increment the ID until an available one is found
+        ++id;
     }
 
     return id;
 }
 
-// Returns current ID as string for easy UI use
-std::string DataManager::GetIDString() {
-    std::string IDString = std::to_string(id);
-    return IDString;
-}
-
-//gets all robot info (i.e., all of the info specified in robot class) for a specified ID
-//used to display necessary information to the user in GUI
+// Gets all robot info for a specified ID
 robots::Robots DataManager::GetAllRobotInfo(int robotId)
 {
-    //temporary placeholder that just creates a robot pre-database integration
-    // robots::Robots clicked_robot(robotId, "Large", 100, 50, "", "Vacuum", 3, "Scrub", 10, 15);
-    robots::Robots clicked_robot = mongo_database.read_robot(robotId);
-    return clicked_robot;
+    return mongo_database.read_robot(robotId);
 }
 
+// Deletes a robot given ID
 void DataManager::DeleteRobot(int robotId)
 {
-    //first remove the robot from the database
+    // Remove the robot from the database
     mongo_database.delete_robot(robotId);
     
-    //then remove the robot from the vector of RobotData
-    //convert id to string (same type as in RobotData struct)
+    // Remove the robot from the RobotManager
+    robot_manager_.remove_robot_by_id(robotId);
+
+    // Remove the robot from the vector of RobotData
     std::string robotIdStr = std::to_string(robotId);
-    //auto allows compiler to take care of type
     for (auto iterator = robots.begin(); iterator != robots.end(); ++iterator) {
         if (iterator->robotID == robotIdStr) {
-            robots.erase(iterator);  // Remove the robot from the vector
-            break;  // Exit the loop after removal
+            robots.erase(iterator);
+            break;
         }
     }
 }
@@ -126,23 +158,15 @@ void DataManager::AddTask(TaskData& task) {
     mongo_database.write_task(robot_id, room_num);
 }
 
-//gets all robots from database, then filters for available robots
-//this part should be fully functional, but right now read_all_robots doesn't include task status string
+// Get available robots for task allocation
 std::vector<robots::Robots> DataManager::GetAvailableRobots()
 {
-    //holds all robots
     std::vector <robots::Robots> robotVector = mongo_database.read_all_robots();
-
-    //holds available robots
     std::vector <robots::Robots> availableRobotVector;
 
-    //iterate through robot vector to find available robots
     for (robots::Robots robot : robotVector) {
-        // std::cout << robot.get_id() << std::endl;
-        // std::cout << robot.get_task_status() << std::endl;
         if (robot.get_task_status() == "Available")
         {
-            // std::cout << "added" << std::endl;
             availableRobotVector.push_back(robot);
         }
     }
