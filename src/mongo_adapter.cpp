b1dec18200e2306f9355f69a91475e7e59be6d81
KayConrad <kay.conrad@richmond.edu>
2024-12-08 20:53:32 -0500

    test

--- src/mongo_adapter.cpp
+++ src/mongo_adapter.cpp
@@ -349,7 +349,7 @@ void adapters::Mongo_Adapter::update_task_status(std::vector<robots::Robots> upd
             auto Room_Number = task_Doc["Room"];
             //If the robot now has an error
             if(update.get_error_status() != ""){
-                std::cout << "1" << std::endl;
+                // std::cout << "1" << std::endl;
                 //add the error to the error table for safe keeping to grab things later. if we are inquiring about an error log
                 db_["error"].insert_one(make_document(
                     kvp("robot_id", update.get_id()),
@@ -426,7 +426,7 @@ void adapters::Mongo_Adapter::update_task_status(std::vector<robots::Robots> upd
 
             }
             else if(update.get_task_status() == "Cancelled" && update.get_error_status() == ""){
-                std::cout << "3" << std::endl;
+                // std::cout << "3" << std::endl;
                 // Find the specific task to update
                 auto task_query_filter = make_document(kvp("robot_id", update.get_id()), kvp("Task Status", "Ongoing"));
 
@@ -458,7 +458,7 @@ void adapters::Mongo_Adapter::update_task_status(std::vector<robots::Robots> upd
                 update_room_availability(Room_Number, "Available");
             }
             else{
-                std::cout << "4" << std::endl;
+                // std::cout << "4" << std::endl;
                 // Find the specific task to update
                 auto task_query_filter = make_document(kvp("robot_id", update.get_id()), kvp("Task Status", "Ongoing"));
 
@@ -490,7 +490,7 @@ void adapters::Mongo_Adapter::update_task_status(std::vector<robots::Robots> upd
                 update_room_availability(Room_Number, "Busy");
             }
         }else{
-            std::cout << "5" << std::endl;
+            // std::cout << "5" << std::endl;
             // If the robot task is available and not ongoing then just update the robot class as only the water and battery level are changing
             auto robot_query_filter = make_document(kvp("_id", update.get_id()));
             auto update_doc = make_document(kvp("$set", make_document(
